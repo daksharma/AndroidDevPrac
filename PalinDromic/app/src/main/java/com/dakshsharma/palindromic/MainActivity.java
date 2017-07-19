85814e2aa78cba9ecd44f1ff89e9a434568d8e6c
Daksh Sharma <dashatrix@gmail.com>
2017-07-19 15:06:26 -0700

    Simple Working Sample

--- PalinDromic/app/src/main/java/com/dakshsharma/palindromic/MainActivity.java
+++ PalinDromic/app/src/main/java/com/dakshsharma/palindromic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dakshsharma.palindromic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpWidgets();
+    }
+
+
+    public void setUpWidgets() {
+        final EditText editText = (EditText) findViewById(R.id.editText);
+        final TextView tv       = (TextView) findViewById(R.id.validCheckTV);
+        final Button   btn      = (Button) findViewById(R.id.checkPalindrome);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String word = editText.getText().toString();
+                tv.setText(isPalindromic(word));
+            }
+        });
+    }
+
+
+    public String isPalindromic(String string) {
+        for (int i = 0, j = string.length() -1; i < j; ++i, --j) {
+            if (string.charAt(i) != string.charAt(j)) {
+                return "False: " + string + " is not Palindrome";
+            }
+        }
+        return "True: " + string + " is Palindromic";
     }
 }
