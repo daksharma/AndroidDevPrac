1a9abaf9525470b7c327887bbc6ddaa9915a0815
Daksh Sharma <dashatrix@gmail.com>
2017-07-19 16:49:46 -0700

    added fizzbuzz

--- PalinDromic/app/src/main/java/com/dakshsharma/palindromic/MainActivity.java
+++ PalinDromic/app/src/main/java/com/dakshsharma/palindromic/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         final EditText editText = (EditText) findViewById(R.id.editText);
         final TextView tv       = (TextView) findViewById(R.id.validCheckTV);
         final Button   btn      = (Button) findViewById(R.id.checkPalindrome);
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,15 +30,34 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(isPalindromic(word));
             }
         });
+
+        final EditText fizzET = (EditText) findViewById(R.id.fizzbuzzET);
+        final TextView fizzTV = (TextView) findViewById(R.id.fizzbuzzTV);
+        final Button fizzBtn = (Button) findViewById(R.id.checkFizzBuzz);
+        fizzBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fizzString = fizzET.getText().toString();
+                fizzTV.setText(dryFizzBuzz(fizzString));
+            }
+        });
     }
 
 
-    public String isPalindromic(String string) {
+    public static String isPalindromic(String string) {
         for (int i = 0, j = string.length() -1; i < j; ++i, --j) {
             if (string.charAt(i) != string.charAt(j)) {
                 return "False: " + string + " is not Palindrome";
             }
         }
         return "True: " + string + " is Palindromic";
+    }
+
+    public static String dryFizzBuzz(String number) {
+        int num = Integer.parseInt(number);
+        if (num % 15 == 0) return num + ": FizzBuzz";
+        else if (num % 3 == 0) return num + ": Fizz";
+        else if (num % 5 == 0) return num + ": Buzz";
+        else return num + " is just is number. -_-";
     }
 }
