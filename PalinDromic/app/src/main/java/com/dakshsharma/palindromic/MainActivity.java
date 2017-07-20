00294b95f42ec5ab7fea0baeae3af197ba298a31
Daksh Sharma <dashatrix@gmail.com>
2017-07-19 17:43:22 -0700

    hide keyboard, empty edit text check

--- PalinDromic/app/src/main/java/com/dakshsharma/palindromic/MainActivity.java
+++ PalinDromic/app/src/main/java/com/dakshsharma/palindromic/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dakshsharma.palindromic;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,8 +29,13 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String word = editText.getText().toString();
-                tv.setText(isPalindromic(word));
+                if (!TextUtils.isEmpty(editText.getText().toString())) {
+                    tv.setText(isPalindromic(editText.getText().toString()));
+
+                } else {
+                    tv.setText("Please enter a word to check Palindrome!");
+                }
+                hideKeyboard(v);
             }
         });
 
@@ -37,16 +45,38 @@ public class MainActivity extends AppCompatActivity {
         fizzBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String fizzString = fizzET.getText().toString();
-                fizzTV.setText(dryFizzBuzz(fizzString));
+                String word = fizzET.getText().toString();
+                if (!TextUtils.isEmpty(word) && TextUtils.isDigitsOnly(word)) {
+                    fizzTV.setText(dryFizzBuzz(word));
+                } else {
+                    fizzTV.setText("Please enter a number!");
+                }
+                hideKeyboard(v);
             }
         });
+
+        final EditText revEditText = (EditText) findViewById(R.id.reverseEditText);
+        final TextView revTextView = (TextView) findViewById(R.id.reverseTextView);
+        final Button revButton = (Button) findViewById(R.id.reverseButton);
+        revButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(revEditText.getText().toString())) {
+                    revTextView.setText(revString(revEditText.getText().toString()));
+                } else {
+                    revTextView.setText("Please enter a word to reverse!");
+                }
+                hideKeyboard(v);
+            }
+        });
+
     }
 
 
     public static String isPalindromic(String string) {
-        for (int i = 0, j = string.length() -1; i < j; ++i, --j) {
-            if (string.charAt(i) != string.charAt(j)) {
+        String palinWord = string.toLowerCase();
+        for (int i = 0, j = palinWord.length() -1; i < j; ++i, --j) {
+            if (palinWord.charAt(i) != palinWord.charAt(j)) {
                 return "False: " + string + " is not Palindrome";
             }
         }
@@ -59,5 +89,22 @@ public class MainActivity extends AppCompatActivity {
         else if (num % 3 == 0) return num + ": Fizz";
         else if (num % 5 == 0) return num + ": Buzz";
         else return num + " is just is number. -_-";
+    }
+
+    public static String revString(String string) {
+        StringBuilder sb = new StringBuilder(string).reverse();
+        String revLowerCase = sb.toString().toLowerCase();
+        StringBuilder result = new StringBuilder();
+        for (String str: revLowerCase.split(" ")) {
+            String rev = str;
+            rev = Character.toUpperCase(rev.charAt(0)) + rev.substring(1);
+            result.append(rev).append(" ");
+        }
+        return result.toString();
+    }
+
+    public void hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
